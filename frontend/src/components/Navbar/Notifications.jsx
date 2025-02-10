fcece5d251c7f3db504195dbcbba66a3d0ac542c
Nguyễn Ngọc Đức <106262560+ngocduc63@users.noreply.github.com>
2025-02-10 19:36:50 +0700

    add antd, update ui notification

--- frontend/src/components/Navbar/Notifications.jsx
+++ frontend/src/components/Navbar/Notifications.jsx
@@ -1,21 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
+import {Dropdown} from 'antd';
+import {likeFillBlack, likeOutline} from "./SvgIcons";
+
+const notifications = [
+    {
+        id: 1,
+        title: "Notification 1",
+        message: "This is notification 1",
+        time: "10 minutes ago",
+        avatar: "https://picsum.photos/200/300"
+    },
+    {
+        id: 2,
+        title: "Notification 2",
+        message: "This is notification 2",
+        time: "30 minutes ago",
+        avatar: "https://picsum.photos/200/301"
+    },
+    {
+        id: 3,
+        title: "Notification 3",
+        message: "This is notification 3",
+        time: "1 hour ago",
+        avatar: "https://picsum.photos/200/302"
+    }
+];
 
 const Notifications = () => {
-  return (
-    <div className="absolute w-56 bg-green-100 rounded  drop-shadow-xl right-96 top-14 border">
-      <div className="absolute right-5 -top-2 rotate-45 h-4 w-4 bg-white rounded-sm border-l border-t"></div>
+    const [open, setOpen] = useState(false);
 
-      <div className="flex flex-col w-full overflow-hidden">
-        {Array(5)
-          .fill("")
-          .map((el) => (
-            <div className="flex items-center gap-3 p-2.5 text-sm pl-4 cursor-pointer hover:bg-gray-50">
-              Profile
+    const handleOpenChange = (newOpen) => {
+        setOpen(newOpen);
+    };
+
+    return (
+        <Dropdown
+            open={open}
+            onOpenChange={handleOpenChange}
+            menu={{
+                items: notifications.map((notification) => ({
+                    key: notification.id,
+                    label: (
+                        <div className="flex items-center">
+                            <img src={notification.avatar} alt="Avatar" className="w-8 h-8 rounded-full mr-2"/>
+                            <div>
+                                <h4>{notification.title}</h4>
+                                <p>{notification.message}</p>
+                                <small>{notification.time}</small>
+                            </div>
+                        </div>
+                    )
+                }))
+            }}
+            trigger={['click']}
+        >
+            <div onClick={(e) => e.preventDefault()} className="relative">
+                <div
+                    className="cursor-pointer sm:block rounded-full p-2 transition duration-300">
+                    {open ? likeFillBlack : likeOutline}
+                </div>
             </div>
-          ))}
-      </div>
-    </div>
-  );
+        </Dropdown>
+    );
 };
 
 export default Notifications;
