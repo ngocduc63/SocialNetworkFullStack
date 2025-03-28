3e1be65d83e7bb9b53b6b492bee5049824f860e0
Nguyễn Ngọc Đức <106262560+ngocduc63@users.noreply.github.com>
2025-03-28 21:33:46 +0700

    update width 80%

--- frontend/src/components/Chats/Inbox.jsx
+++ frontend/src/components/Chats/Inbox.jsx
@@ -202,7 +202,7 @@ const Inbox = () => {
     <>
       <MetaData title="Instagram • Chats" />
 
-      <div className="mt-14 sm:mt-[4.7rem] pb-4 rounded h-[90vh] xl:w-2/3 mx-auto sm:pr-14 sm:pl-8">
+      <div className="mt-14 sm:mt-[4.7rem] pb-4 rounded h-[90vh] xl:w-4/5 mx-auto sm:pr-14 sm:pl-8">
         <div className="flex border h-full rounded w-full bg-white">
           {/* sidebar */}
           <Sidebar openModal={openModal} />
