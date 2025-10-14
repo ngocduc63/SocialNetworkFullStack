04e4944aca6e6e76a867b489db71eecef399422b
Nguyễn Ngọc Đức <duco060303@gmail.com>
2025-10-14 22:16:49 +0700

    update fe domain config

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://yourfrontend.com"],
+    origin: ["http://localhost:3000", "https://pollux-git-devdeploy-ducdo63s-projects.vercel.app"],
     credentials: true,
     methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
