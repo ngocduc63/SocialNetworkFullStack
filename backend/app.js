83a7e37768bf8fdfb69d317325bedbee0c42deff
bachtv <tts_bachtv@viettelsoftware.com>
2026-01-17 12:53:30 +0700

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://pollux-opal.vercel.app" ],
+    origin: ["http://localhost:3000", "https://pollux-opal.vercel.app", "https://social-network-bops.vercel.app" ],
     credentials: true,
     methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
